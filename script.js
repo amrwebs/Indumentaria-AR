61ea4dd9ab1da8aa031d2c68b1e3707ccc26481a
amrweb <130936781+amrwebs@users.noreply.github.com>
2025-04-20 08:45:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const productos = [
   { id: 1, nombre: "Camiseta ROMA 24/25", precio: 46000, imagen: "img/roma1.jpg", link: "productos/producto1/producto1.html" },
   { id: 2, nombre: "Camiseta AFA SUPLENTE 24/25", precio: 46000, imagen: "img/AFA-SUPLENTE-25.JPEG", link: "productos/producto1/producto2.html" },
   { id: 3, nombre: "Camiseta ATLETICO DE MADRID", precio: 46000, imagen: "img/ATLETICO1.JPEG", link: "productos/producto1/producto3.html" },
-  { id: 4, nombre: "Camiseta PALMEIRAS", precio: 46000, imagen: "img/palma,jpg", link: "productos/producto1/producto4.html" }
+  { id: 4, nombre: "Camiseta PALMEIRAS", precio: 46000, imagen: "img/palma.jpg", link: "productos/producto1/producto4.html" }
 ];
 
 const productList = document.getElementById("product-list");
