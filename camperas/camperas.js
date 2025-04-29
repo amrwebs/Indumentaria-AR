26c8219db406d411ce9ea86de64d2bb5430b0ea9
amrweb <130936781+amrwebs@users.noreply.github.com>
2025-04-28 23:26:54 -0300

    Update camperas.js

--- camperas/camperas.js
+++ camperas/camperas.js
@@ -5,7 +5,7 @@ const productos = [
     { id: 4, nombre: "Campera RIVER ROMPEVIENTO SALMÓN", precio: 41000, imagen: "img/camp-river-25.JPEG", link: "camperas/camp-river-salmon.html" },
     { id: 5, nombre: "Campera AFA ROMPEVIENTO DORADA", precio: 41000, imagen: "img/camp-afa1-25.JPEG", link: "camperas/camp-arg-dor.html" },
     { id: 6, nombre: "Campera AFA SEGUNDO UNIFORME 1994 (SIN STOCK)", precio: 41000, imagen: "img/sinstock.jpg", link: "" },
-    { id: 7, nombre: "Campera AFA ROMPEVIENTO NEGRA", precio: 41000, imagen: "img/camp-afa2-25.JPEGg", link: "camperas/camp-arg-negra.html" },
+    { id: 7, nombre: "Campera AFA ROMPEVIENTO NEGRA", precio: 41000, imagen: "img/camp-afa2-25.JPEG", link: "camperas/camp-arg-negra.html" },
     { id: 8, nombre: "Campera BOCA ROMPEVIENTO AZUL OSCURO", precio: 41000, imagen: "img/boca40camp.JPEG", link: "camperas/camp-boca-azul.html" },
     { id: 9, nombre: "Campera RIVER ROMPEVIENTO GRIS MATE", precio: 41000, imagen: "img/river40camp.JPEG", link: "camperas/camp-river-gris.html" }
   ];
